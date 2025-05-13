cab5842cf4aa3958a1724f0bf6c1d52338aada10
hisa <hisayuki.k8@gmail.com>
2025-05-13 17:28:26 +0900

    remove eslint && add biome

---
+++ src/libs/firebase/index.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
